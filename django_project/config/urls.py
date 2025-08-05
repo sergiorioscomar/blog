c73c9bc5330858744f123839d823a0f1491ed623
Sergio Rios <serggio.rs@gmail.com>
2025-08-05 09:45:25 -0300

    update views and urls

--- django_project/config/urls.py
+++ django_project/config/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.saludar),
+    path('', views.inicio),
     # Urls de app blog
     path('blog/', include('apps.blog.urls')),
 
