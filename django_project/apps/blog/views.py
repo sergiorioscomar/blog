505987ad4a825780fcb9720e96fc4bacf4e25771
Sergio Rios <serggio.rs@gmail.com>
2025-08-05 21:49:59 -0300

    #14 | filtros de post mejoras implementacion

--- django_project/apps/blog/views.py
+++ django_project/apps/blog/views.py
@@ -1,21 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Post, User, Comentario
-
-
-# ----------- Vistas Basadas en Clases--------------------------------
-
+from django.urls import reverse_lazy
+from django.forms import modelform_factory
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
-# listar todos los posteos
+from .models import Post, User, Comentario
+from .forms import CreatePostForm, UpdatePostForm
+
+
+# ----------- Vistas Basadas en Clases --------------------------------
+
+# LISTA DE POSTS CON BÚSQUEDA Y FILTRO
 class PostListView(ListView):
     model = Post
     template_name = "post_list.html"
     context_object_name = "posts"
 
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        query = self.request.GET.get('q')
+        categoria = self.request.GET.get('categoria')
 
-from django.forms import modelform_factory
-# obtener un posteo especifico por pk
+        # Filtro de búsqueda por título o contenido
+        if query:
+            queryset = queryset.filter(titulo__icontains=query) | queryset.filter(contenido__icontains=query)
+
+        # Filtro por categoría
+        if categoria and categoria != "todas":
+            queryset = queryset.filter(categoria=categoria)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Pasamos las categorías únicas al template
+        context['categorias'] = Post.objects.values_list('categoria', flat=True).distinct()
+        return context
+
+
+# DETALLE DE POST + FORMULARIO DE COMENTARIOS
 class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
@@ -23,36 +46,28 @@ class PostDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        ComentarioForm = modelform_factory(Comentario, fields = ['contenido'])
+        ComentarioForm = modelform_factory(Comentario, fields=['contenido'])
         context['form'] = ComentarioForm()
         return context
 
 
-# eliminar posteos
-from django.urls import reverse_lazy
-
+# ELIMINAR POST (VBC)
 class PostDeleteView(DeleteView):
     model = Post
     template_name = "post_confirm_delete.html"
     success_url = reverse_lazy("post-list")
 
-    
 
-#  delete en VBF
-from django.shortcuts import get_object_or_404, redirect
-
+# ELIMINAR POST (VBF)
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         post.delete()
         return redirect("post-list")
-
     return render(request, "post_confirm_delete.html", {"post": post})
 
 
-# ---------------------------- Vistas Comentarios---------------------------------------------
-
-
+# CREAR COMENTARIO
 class ComentarioCreateView(CreateView):
     model = Comentario
     fields = ['contenido']
@@ -61,16 +76,13 @@ class ComentarioCreateView(CreateView):
     def form_valid(self, form):
         form.instance.autor = self.request.user
         form.instance.post_id = self.kwargs['pk']
-
         return super().form_valid(form)
-    
+
     def get_success_url(self):
         return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['pk']})
-    
-# _________________________ posteos fuera del admin___________________
 
-from .forms import CreatePostForm, UpdatePostForm
 
+# CREAR POST
 class PostCreateView(CreateView):
     model = Post
     form_class = CreatePostForm
@@ -82,14 +94,15 @@ class PostCreateView(CreateView):
         return super().form_valid(form)
 
 
+# ACTUALIZAR POST
 class PostUpdateView(UpdateView):
     model = Post
     form_class = UpdatePostForm
-    template_name ="post_update_form.html"
+    template_name = "post_update_form.html"
     success_url = reverse_lazy("post-list")
 
-# Página de inicio pública
+
+# PÁGINA DE INICIO
 def index(request):
-    from .models import Post
     ultimos_posts = Post.objects.order_by('-fecha_creacion')[:3]
     return render(request, 'index.html', {'ultimos_posts': ultimos_posts})
