e8c89fcbe81aea2f068d1b8dbf416fe31a950152
Sergio Rios <serggio.rs@gmail.com>
2025-08-10 18:25:11 -0300

    quito comentario en base por el middleware csrfview

--- django_project/config/settings/base.py
+++ django_project/config/settings/base.py
@@ -36,7 +36,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware', una vez se comienza a desarrollar quitar comentario
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
