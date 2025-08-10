6d633705a4ccf5ee9c50fb6ebd2df0aaa7cabd1f
Sergio Rios <serggio.rs@gmail.com>
2025-08-10 20:14:24 -0300

    #10 | mejoras visuales login register y recupasswd

--- django_project/apps/blog/models.py
+++ django_project/apps/blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
-# modelo de categorias
+# modelo de categorias# Create your models here.
+
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
