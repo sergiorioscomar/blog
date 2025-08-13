1fa37188c2d24ae93c11b3577338a13ac7a4e857
Sergio Rios <serggio.rs@gmail.com>
2025-08-13 11:27:16 -0300

    #1 Mejora documentacion del proyecto

--- django_project/apps/accounts/forms.py
+++ django_project/apps/accounts/forms.py
@@ -9,16 +9,10 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email"]
-        widgets = {f: forms.TextInput(attrs={"class": "form-control"}) for f in ["first_name","last_name","email"]}
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["avatar", "bio", "website", "github"]
-        widgets = {
-            "avatar": forms.ClearableFileInput(attrs={"class": "form-control"}),
-            "bio": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
-            "website": forms.URLInput(attrs={"class": "form-control"}),
-            "github": forms.URLInput(attrs={"class": "form-control"}),
-        }
+
         
